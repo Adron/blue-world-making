6d1b28957de53ae82ef0debc7935d8fa2edf8cd8
Adron Hall <adronhall@gmail.com>
2017-08-27 23:26:42 -0700

    Cleanup of bits. Rolling shkeymessup.

--- phase1/azure_kubernetes.tf
+++ phase1/azure_kubernetes.tf
@@ -18,7 +18,7 @@ resource "azurerm_container_service" "kubyhousecontainers" {
     admin_username = "${var.linux_admin_username}"
 
     ssh_key {
-      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCaRP6U+UwBP6LRelSsO+tgzZGul5UNHiGMy0GlSI/CG4d8jpdt4OIbZLsWtFsyiyTyVhBzmShiZZh+V2j9vFojqPTTK34KBj35vo1EKwTgLFEDBvsDFZzjc7SrQZ/DuHN6eJnv2ot9+fSJpfB7tg8m9OUEnFs8oZFrAq+qOmT5DWFpXwIo9VgcisDHack9bTFVABq2nnHWex4Y1NA8xtM0Q/FQulQ2jX8iVRCVbEOcRE6IVnEaPMLol+7ilX/eW0Ojgg4Y5e8LBXqUGUuXQyURPfOXuaYJrZqGMrMNflrAiufh6HRXceT8/v4ExJGQvJdPnwyxIl1Dxv2HRYt0ImZhRxFvpw1+UsBgYc6Gfi3icBTsXuJDZkhIE2lL2qWiFKEsvV4zXbORjDM7EHvGk1uunVdvfJ22CpLC/7QI7VQtd3e7mBa1t+mSdkKgN121h5xkpDVuPViUMob7HiwlCNI81PowfYeuZx2S8ZOqjHTg9UEh2Q3IK7k5ZiAL7zrWpTOwID+H1BfgKJmeXfVW4v9iMOoZk9OKTXz5qACZtdntV21bxRyXUUHssTUbdpw3f+MQlKIEstvNI6BC7fTLVK1VXlEcDrfHAgNjDQi55iE0p8JrZU5rwkJBHhVSJlM6uG3rLAssyOLu2Qklltt1kxMIEg7jIKLIi1yVtEKkz2WhWQ== adronhall@gmail.com"
+      key_data = ""
     }
   }
 
@@ -43,6 +43,7 @@ resource "azurerm_container_service" "kubyhousecontainers" {
   }
 }
 
+// Output variables
 output "master_fqdn" {
   value = "${azurerm_container_service.kubyhousecontainers.master_profile.fqdn}"
 }
@@ -50,3 +51,4 @@ output "master_fqdn" {
 output "ssh_command_master0" {
   value = "ssh ${var.linux_admin_username}@${azurerm_container_service.kubyhousecontainers.master_profile.fqdn} -A -p 22"
 }
+
