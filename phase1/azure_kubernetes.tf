b6d6cb88c401abd235e7bc176ff908770491cef0
Adron Hall <adronhall@gmail.com>
2017-09-01 15:33:32 -0700

    Final changes for the day.

--- phase1/azure_kubernetes.tf
+++ phase1/azure_kubernetes.tf
@@ -1,49 +1,39 @@
-resource "azurerm_container_service" "kubyhousecontainers" {
-  name                   = "kubecontainers"
-  location               = "${azurerm_resource_group.kubernetes_house.location}"
-  resource_group_name    = "${azurerm_resource_group.kubernetes_house.name}"
-  orchestration_platform = "Kubernetes"
-
-  master_profile {
-    count      = 1
-    dns_prefix = "kubyhouse"
-  }
-
-  linux_profile {
-    admin_username = "${var.linux_admin_username}"
-
-    ssh_key {
-      key_data = "${var.azure_ssh_key}"
-    }
-  }
-
-  agent_pool_profile {
-    name       = "default"
-    count      = "${var.azure_cluster_count}"
-    dns_prefix = "kuby.house"
-    vm_size    = "Standard_A0"
-  }
-
-  service_principal {
-    client_id       = "${var.client_id}"
-    client_secret   = "${var.client_secret}"
-  }
-
-  diagnostics_profile {
-    enabled = false
-  }
-
-  tags {
-    Environment = "Production"
-  }
-}
-
-// Output variables
-output "master_fqdn" {
-  value = "${azurerm_container_service.kubyhousecontainers.master_profile.fqdn}"
-}
-
-output "ssh_command_master0" {
-  value = "ssh ${var.linux_admin_username}@${azurerm_container_service.kubyhousecontainers.master_profile.fqdn} -A -p 22"
-}
-
+//resource "azurerm_container_service" "kubyhousecontainers" {
+//  name                   = "kubecontainers"
+//  location               = "${azurerm_resource_group.kubernetes_house.location}"
+//  resource_group_name    = "${azurerm_resource_group.kubernetes_house.name}"
+//  orchestration_platform = "Kubernetes"
+//
+//  master_profile {
+//    count      = 1
+//    dns_prefix = "kubyhouse"
+//  }
+//
+//  linux_profile {
+//    admin_username = "${var.linux_admin_username}"
+//
+//    ssh_key {
+//      key_data = "${var.azure_ssh_key}"
+//    }
+//  }
+//
+//  agent_pool_profile {
+//    name       = "default"
+//    count      = "${var.azure_cluster_count}"
+//    dns_prefix = "kuby.house"
+//    vm_size    = "Standard_A0"
+//  }
+//
+//  service_principal {
+//    client_id       = "${var.client_id}"
+//    client_secret   = "${var.client_secret}"
+//  }
+//
+//  diagnostics_profile {
+//    enabled = false
+//  }
+//
+//  tags {
+//    Environment = "Production"
+//  }
+//}
