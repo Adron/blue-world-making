2f7e374852cc4c16331bfd59227425ff1a62b947
Adron Hall <adronhall@gmail.com>
2017-09-19 12:31:02 -0700

    Adding appropriately parameters.

--- review.sh
+++ review.sh
@@ -3,3 +3,19 @@
 CLUSTER_USERNAME="coolusername"
 CLUSTER_PASSWORD="coolpassword"
 CLUSTER_NAME="kubyhouse"
+
+cd phase1
+echo "Stage 1 Plan"
+terraform init
+terraform plan  \
+    -var linux_admin_username=$CLUSTER_USERNAME \
+    -var linux_admin_password=$CLUSTER_PASSWORD \
+    -var cluster_name=$CLUSTER_NAME
+
+cd ../phase2
+echo "Stage 2 Plan"
+terraform init
+terraform plan \
+    -var linux_admin_username=$CLUSTER_USERNAME \
+    -var linux_admin_password=$CLUSTER_PASSWORD \
+    -var cluster_name=$CLUSTER_NAME
