30108705c10b2909d67298085fa60a45ef36886e
Adron Hall <adronhall@gmail.com>
2017-09-22 14:45:35 -0700

    Cleaned up again.

--- obliteration.sh
+++ obliteration.sh
@@ -11,9 +11,9 @@ terraform init
 echo "Starting phase 1 destruction. Destroying the core infrastructure for Kubernetes."
 
 echo "terraform destroy \
--var linux_admin_username=$CLUSTER_USERNAME \
--var linux_admin_password=$CLUSTER_PASSWORD \
--var cluster_name=$CLUSTER_NAME"
+  -var linux_admin_username=$CLUSTER_USERNAME \
+  -var linux_admin_password=$CLUSTER_PASSWORD \
+  -var cluster_name=$CLUSTER_NAME"
 
 terraform destroy \
     -var linux_admin_username=$CLUSTER_USERNAME \
